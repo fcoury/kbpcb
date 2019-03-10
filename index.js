bccf7394e0b99969767d666a796b6739b407251d
Felipe Coury <felipe.coury@gmail.com>
2019-03-10 15:18:42 -0300

    feat: add footprints and initial error handling

--- index.js
+++ index.js
@@ -17,36 +17,44 @@ app.use(bodyParser.raw({limit: '50mb'}));
 
 app.post('/submit', (req, res) => {
   new formidable.IncomingForm().parse(req, (err, fields, files) => {
-    if (err) {
-      console.error('Error', err)
-      throw err
+    try {
+      if (err) {
+        console.error('Error', err)
+        throw err
+      }
+      if (files.document.type !== 'application/json') {
+        return res.redirect('/?error=Only+JSON+files+accepted');
+      }
+      console.log('files.document', files.document.toJSON());
+      const { document } = files;
+      const docName = path.basename(document.name).split('.')[0];
+      const data = JSON.parse(fs.readFileSync(document.path, 'utf8'));
+      const { layout } = parseLayout(data, docName);
+      const name = fields.name || docName;
+      console.log('fields', fields);
+      const borders = {
+        edge:    fields.borderSpacing,
+        corners: fields.borderRound,
+      };
+
+      const zipFiles = [
+        [`${name}.pro`, fs.readFileSync('templates/project.pro')],
+        [`${name}.sch`, genSchematics(layout)],
+        [`${name}.kicad_pcb`, genPCB(layout, borders)],
+        [`sym-lib-table`, fs.readFileSync('templates/sym-lib-table')],
+        [`fp-lib-table`, fs.readFileSync('templates/fp-lib-table')],
+      ];
+      addFolder(zipFiles, 'kicad_lib_tmk');
+      addFolder(zipFiles, 'keyboard_parts.pretty');
+      addFolder(zipFiles, 'MX_Alps_Hybrid.pretty');
+
+      zip = makeZip(res, zipFiles);
+
+      res.setHeader('Content-disposition', `attachment; filename=${name}-pcb.zip`);
+    } catch (err) {
+      console.log('err', err);
+      res.status(500).send(`<html><body><p>Error in ${req.method} ${req.url}<p><pre>${err.stack}</pre></body></html>`);
     }
-    console.log('files.document', files.document.toJSON());
-    const { document } = files;
-    const docName = path.basename(document.name).split('.')[0];
-    const data = JSON.parse(fs.readFileSync(document.path, 'utf8'));
-    const { layout } = parseLayout(data, docName);
-    const name = fields.name || docName;
-    console.log('fields', fields);
-    const borders = {
-      edge:    fields.borderSpacing,
-      corners: fields.borderRound,
-    };
-
-    const zipFiles = [
-      [`${name}.pro`, fs.readFileSync('templates/project.pro')],
-      [`${name}.sch`, genSchematics(layout)],
-      [`${name}.kicad_pcb`, genPCB(layout, borders)],
-      [`sym-lib-table`, fs.readFileSync('templates/sym-lib-table')],
-      [`fp-lib-table`, fs.readFileSync('templates/fp-lib-table')],
-    ];
-    addFolder(zipFiles, 'kicad_lib_tmk');
-    addFolder(zipFiles, 'keyboard_parts.pretty');
-    addFolder(zipFiles, 'MX_Alps_Hybrid.pretty');
-
-    zip = makeZip(res, zipFiles);
-
-    res.setHeader('Content-disposition', `attachment; filename=${name}-pcb.zip`);
   });
 });
 
